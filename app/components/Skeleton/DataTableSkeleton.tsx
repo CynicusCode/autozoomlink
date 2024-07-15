c73c154925aa318037e0e611873628aeb9c717bc
CynicusCode <114907376+CynicusCode@users.noreply.github.com>
2024-07-15 00:59:43 -0600

    add - better skeleton to datatable.

--- app/components/Skeleton/DataTableSkeleton.tsx
+++ app/components/Skeleton/DataTableSkeleton.tsx
@@ -1,4 +1,3 @@
-// app/components/Skeleton/DataTableSkeleton.tsx
 import { Skeleton } from "@/components/ui/skeleton";
 import type React from "react";
 
@@ -10,17 +9,24 @@ const DataTableSkeleton: React.FC<{ rows?: number; columns?: number }> = ({
 	const rowsArray = Array(rows).fill(0);
 
 	return (
-		<div className="space-y-2">
-			{rowsArray.map((_, rowIndex) => (
-				<div key={`row-${rowIndex}-${Date.now()}`} className="flex space-x-2">
-					{columnsArray.map((_, colIndex) => (
-						<Skeleton
-							key={`col-${colIndex}-${Date.now()}`}
-							className="h-8 w-full"
-						/>
-					))}
-				</div>
-			))}
+		<div>
+			<div className="flex space-x-2 mb-4">
+				<Skeleton className="h-9 w-2/4" />
+				<Skeleton className="h-9 w-1/4" />
+				<Skeleton className="h-9 w-48" />
+			</div>
+			<div className="space-y-2">
+				{rowsArray.map((_, rowIndex) => (
+					<div key={`row-${rowIndex}-${Date.now()}`} className="flex space-x-2">
+						{columnsArray.map((_, colIndex) => (
+							<Skeleton
+								key={`col-${colIndex}-${Date.now()}`}
+								className="h-8 w-full"
+							/>
+						))}
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
