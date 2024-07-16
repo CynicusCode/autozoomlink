bcc5d47df54d706b8a690cc793f31cd9ff361d94
CynicusCode <114907376+CynicusCode@users.noreply.github.com>
2024-07-16 00:50:46 -0600

    update - remove test pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 //page.tsx
 import AutoLinkGenerator from "./components/Link-Generator/AutoLinkGenerator";
-import { LinkGenerator } from "./components/Link-Generator/LinkGenerator"; // Note: This import doesn't seem to be used in this file.
 import { Navbar } from "./components/shared/Navbar";
 import React from "react";
 
